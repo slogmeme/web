b06ffd19a42ad5b40b8804fbc553529e49a5f667
zoosos <wbin90620@gmail.com>
2025-01-09 07:15:24 -0800

    remove unused variable or library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from 'next/font/local';
 import "./globals.css";
-import Head from "next/head";
 import { GoogleTagManager } from "@next/third-parties/google"
 import Script from "next/script";
 
