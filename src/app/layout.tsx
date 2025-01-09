5d0f1417922d4387d13f715c0d81265c93c3eef6
zoosos <wbin90620@gmail.com>
2025-01-09 07:13:58 -0800

    google, facebook metadata added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import localFont from 'next/font/local';
 import "./globals.css";
 import Head from "next/head";
+import { GoogleTagManager } from "@next/third-parties/google"
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,43 +26,46 @@ export const metadata: Metadata = {
   description: "3D AI Blockchain Cricket.",
 };
 
-    export default function RootLayout({
-      children,
-    }: Readonly<{
-      children: React.ReactNode;
-    }>) {
-      return (
-        <html lang="en">
-          <Head>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-E1YW5274VY"></script>
-            <script dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-E1YW5274VY');
-              `,
-            }} />
-            <script dangerouslySetInnerHTML={{
-              __html: `
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window,document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '366194547064903'); 
-                fbq('track', 'PageView');
-              `,
-            }} />
-          </Head>
-          <body
-            className={`${geistSans.variable} ${geistMono.variable} ${goodSans.className} antialiased`}
-          >
-            {children}
-          </body>
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <GoogleTagManager gtmId="G-E1YW5274VY" />
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(f, b, e, v, n, t, s) {
+              if (f.fbq) return;
+              n = f.fbq = function() {
+                n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+              };
+              if (!f._fbq) f._fbq = n;
+              n.push = n;
+              n.loaded = true;
+              n.version = '2.0';
+              n.queue = [];
+              t = b.createElement(e);
+              t.async = true;
+              t.src = v;
+              s = b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t, s);
+            })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '366194547064903');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${goodSans.className} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
